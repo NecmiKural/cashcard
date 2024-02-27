30b63b5c5f982fb1cf1d13d8444b59976064f894
neo <necmibkural@gmail.com>
2024-02-27 09:48:59 +0300

    post implemented

--- src/main/java/example/cashcard/CaschCardController.java
+++ src/main/java/example/cashcard/CaschCardController.java
@@ -1,12 +1,16 @@
 package example.cashcard;
 
 import java.util.Optional;
+import java.net.URI;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -22,12 +26,17 @@ class CashCardController {
         // optional makes it there can be data or not, smart object
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
 
-
         // if there is data
-        if (cashCardOptional.isPresent()) {
-            return ResponseEntity.ok(cashCardOptional.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
